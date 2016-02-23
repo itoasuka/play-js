61a32be8e330c0b79dbe9dd2c7627e5407edc574
itoasuka <itoasuka@gmail.com>
2016-02-23 11:30:05 +0900

    webpackのホットモジュール入れ替えがあまり使いでがなさそうだったのでその関連を削除する。

--- ui/app/webpack/store/confitureStore.js
+++ ui/app/webpack/store/confitureStore.js
@@ -7,27 +7,9 @@ const finalCreateStore = compose(
   typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' && __DEV__ ? window.devToolsExtension() : (f) => f
 )(createStore);
 
-function devConfigureStore(initialState) {
-  const store = finalCreateStore(rootReducer, initialState);
-
-  if (__DEV__ && module.hot) {
-    // Webpack のホットモジュール入れ替えが有効ならそれを使う
-    module.hot.accept('../reducers', () => {
-      const nextReducer = require('../reducers').default;
-      store.replaceReducer(nextReducer);
-    });
-  }
-
-  return store;
-}
-
-function prodConfigureStore(initialState) {
+export default function configureStore(initialState) {
   const store = finalCreateStore(rootReducer, initialState);
 
   return store;
 }
-
-const configureStore = (__DEV__) ? devConfigureStore : prodConfigureStore;
-
-export default configureStore;
 
